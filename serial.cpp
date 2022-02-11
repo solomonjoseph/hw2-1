065d119f62072447771e634d9e39be1f7512ec91
Solomon <solthums@gmail.com>
2022-02-10 22:28:27 -0800

    Initial commit

--- serial.cpp
+++ serial.cpp
@@ -1,5 +1,13 @@
 #include "common.h"
 #include <cmath>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+int num_bins;
+double bin_size;
+
+vector<vector<set<int>>> bins;
 
 // Apply the force from neighbor to particle
 void apply_force(particle_t& particle, particle_t& neighbor) {
@@ -19,6 +27,27 @@ void apply_force(particle_t& particle, particle_t& neighbor) {
     double coef = (1 - cutoff / r) / r2 / mass;
     particle.ax += coef * dx;
     particle.ay += coef * dy;
+}
+
+void apply_force_bin(particle_t* parts, int bin_x, int bin_y) {
+    set<int>::iterator itr_i, itr_j;
+    set<int> bin = bins[bin_x][bin_y];
+    for (itr_i = bin.begin(); itr_i != bin.end(); itr_i++) {
+        for (itr_j = bin.begin(); itr_j != bin.end(); itr_j++) {
+            apply_force(parts[*itr_i], parts[*itr_j]);
+        }
+    }
+}
+
+void apply_force_between_bins(particle_t* parts, int bin_one_x, int bin_one_y, int bin_two_x, int bin_two_y) {
+    set<int>::iterator itr_i, itr_j;
+    set<int> bin_one = bins[bin_one_x][bin_one_y];
+    set<int> bin_two = bins[bin_two_x][bin_two_y];
+    for (itr_i = bin_one.begin(); itr_i != bin_one.end(); itr_i++) {
+        for (itr_j = bin_two.begin(); itr_j != bin_two.end(); itr_j++) {
+            apply_force(parts[*itr_i], parts[*itr_j]);
+        }
+    }
 }
 
 // Integrate the ODE
@@ -47,19 +76,47 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
 	// You can use this space to initialize static, global data objects
     // that you may need. This function will be called once before the
     // algorithm begins. Do not do any particle simulation here
+    bin_size = max(min_r, (int) sqrt(cutoff / density));
+    num_bins = size / bin_size;
+    bins = vector<vector<set<int>>>(num_bins, vector<set<int>>(num_bins, set<int>()));
+    for (int i = 0; i < num_parts; i++) {
+        int x_bin = std::min(num_bins - 1, (int) (parts[i].x / BIN_SIZE));
+        int y_bin = std::min(num_bins - 1, (int) (parts[i].y / BIN_SIZE));
+        bins[x_bin][y_bin].insert(i);
+    }
 }
+
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // Compute Forces
-    for (int i = 0; i < num_parts; ++i) {
-        parts[i].ax = parts[i].ay = 0;
-        for (int j = 0; j < num_parts; ++j) {
-            apply_force(parts[i], parts[j]);
+    for (int i = 0; i < num_bins; i++) {
+        for (int j = 0; j < num_bins; j++) {
+            apply_force_bin(parts, i, j);
+            if (j > 0) {
+                apply_force_between_bins(parts, i, j, i, j-1);
+                if (i > 0) {
+                    apply_force_between_bins(parts, i, j, i-1, j);
+                    apply_force_between_bins(parts, i, j, i-1, j-1);
+                }
+                if (i < num_bins - 1) {
+                    apply_force_between_bins(parts, i, j, i+1, j-1);
+                }
+            }
         }
     }
 
     // Move Particles
     for (int i = 0; i < num_parts; ++i) {
+        double x_old = parts[i].x;
+        double y_old = parts[i].y;
         move(parts[i], size);
+        if (x_old != parts[i].x || y_old != parts[i].y) {
+            int x_bin_old = std::min(num_bins - 1, (int) (x_old / BIN_SIZE));
+            int y_bin_old = std::min(num_bins - 1, (int) (y_old / BIN_SIZE));
+            int x_bin_new = std::min(num_bins - 1, (int) (parts[i].x / BIN_SIZE));
+            int y_bin_new = std::min(num_bins - 1, (int) (parts[i].y / BIN_SIZE));
+            bins[x_bin_old][y_bin_old].erase(i);
+            bins[x_bin_new][y_bin_new].insert(i);
+        }
     }
 }
